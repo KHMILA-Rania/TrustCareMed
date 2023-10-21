10ad57e6c43e4df59094b5d11e3e1ea16c5c3993
KHMILA-Rania <rania.khmila@gmail.com>
2023-10-21 06:02:44 -0700

    fixing navbar

--- trustCareMed_front/src/app/app-routing.module.ts
+++ trustCareMed_front/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 const routes: Routes = [
-  { path: '/home', component: HomePageComponent},
+  { path: 'home', component: HomePageComponent},
   { path: 'home/signup', component: SignUpComponent}
 ];
 
