17851484517525a1fa738df3e8ad9fd69a0a8d45
KHMILA-Rania <rania.khmila@gmail.com>
2023-11-17 08:02:05 -0800

    added app routing

--- trustCareMed_front/src/app/app-routing.module.ts
+++ trustCareMed_front/src/app/app-routing.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { HomePageComponent } from './user/components/home-page/home-page.component';
 import { SignUpComponent } from './user/components/sign-up/sign-up.component';
-const routes: Routes = [
-  { path: 'home', component: HomePageComponent},
-  { path: 'home/signup', component: SignUpComponent},
-=======
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { AboutComponent } from './components/about/about.component';
-
 const routes: Routes = [
   { path: 'home', component: HomePageComponent},
   { path: 'home/signup', component: SignUpComponent},
-  { path: 'home/contact', component: ContactComponent},
-  { path: 'home/about', component : AboutComponent},
-  
->>>>>>> 754283bbeb104c1d7cda95c6d49d8d714a464373
+
   
 ];
 
