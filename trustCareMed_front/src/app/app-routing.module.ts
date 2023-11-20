db54230233d3c52e81d8726dc82a84d36d887048
KHMILA-Rania <rania.khmila@gmail.com>
2023-11-20 03:19:13 -0800

    bbb

--- trustCareMed_front/src/app/app-routing.module.ts
+++ trustCareMed_front/src/app/app-routing.module.ts
@@ -1,15 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-import { HomePageComponent } from './user/components/home-page/home-page.component';
-import { SignUpComponent } from './user/components/sign-up/sign-up.component';
-import { ContactComponent } from './components/contact/contact.component';
-const routes: Routes = [
-  { path: 'home', component: HomePageComponent},
-  { path: 'home/signup', component: SignUpComponent},
-
-=======
 import { HomePageComponent } from './user/components/home-page/home-page.component';
 import { SignUpComponent } from './user/components/sign-up/sign-up.component';
 import { ContactComponent } from './user/components/contact/contact.component';
@@ -22,7 +12,6 @@ const routes: Routes = [
   {path:'home/contact', component:ContactComponent},
   {path:'home/sign-in', component:SignInComponent}
   
->>>>>>> users/jasser
   
 ];
 
